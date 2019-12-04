04e6d8922c104fa40c6f2253eb5a49f47732cd83
wangpin34 <270280522@qq.com>
2019-12-04 14:20:06 +0800

    fix: Add default stack key value, add function to read the key from both package.json and cmd

--- index.js
+++ index.js
@@ -1,73 +1,82 @@
 #! /usr/bin/env node
-const dotenv = require('dotenv')
-const path = require('path')
-const args = require('args')
-const { exec } = require('child_process')
+const dotenv = require("dotenv");
+const path = require("path");
+const args = require("args");
+const { exec } = require("child_process");
 
-const cwd = process.cwd()
-const pkg = require(path.join(cwd, './package.json'))
+const cwd = process.cwd();
+const pkg = require(path.join(cwd, "./package.json"));
 
-const conf = pkg.senv || { stack: 'stack' }
-const stack = conf.stack
+const conf = pkg.senv;
 
-function load(file) {
+function load(file, inlineKey) {
   if (file) {
-    console.log(`Load env from ${file}`)
-    return dotenv.config({ path: path.join(cwd, file) })
+    console.log(`Loaded env variables from ${file}`);
+    return dotenv.config({ path: path.join(cwd, file) });
   }
-  if (process.env[stack]) {
-    const target = path.join(cwd, `.senv.${process.env[stack]}`)
-    console.log(`Load env from ${target}`)
-    return dotenv.config({ path: target })
+  const key = inlineKey || (conf && conf.key) || "stack";
+  if (process.env[key]) {
+    const target = path.join(cwd, `.senv.${process.env[key]}`);
+    console.log(`Loaded env variables from ${target}`);
+    return dotenv.config({ path: target });
   }
-  console.log(`Load env from ${path.join(cwd, `.senv`)}`)
-  return dotenv.config({ path: path.join(cwd, `.senv`) })
+  console.log(
+    `Loaded env variables from ${path.join(
+      cwd,
+      `.senv`
+    )} for default since \'stack\' is not assigned`
+  );
+  return dotenv.config({ path: path.join(cwd, `.senv`) });
 }
 
 function cmd(flags) {
-  const { cmd, file } = flags
-  const result = load(file)
+  const { cmd, file } = flags;
+  const result = load(file, flags.key);
   if (flags.verbose) {
-    console.log(`Loading command: ${cmd}`)
-    console.log(`env variables:\n${JSON.stringify(result.parsed, null, 2)}`)
+    console.log(`Loading command: ${cmd}`);
+    console.log(`env variables:\n${JSON.stringify(result.parsed, null, 2)}`);
   }
-  const child = exec(cmd, {windowsHide: true, maxBuffer: 1024 * 1024}, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`exec error: ${err}`)
-      return
+  const child = exec(
+    cmd,
+    { windowsHide: true, maxBuffer: 1024 * 1024 },
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
     }
-    console.log(`stdout: ${stdout}`)
-    console.log(`stderr: ${stderr}`)
-  })
+  );
 
-  child.stdout.on('data', function(data) {
-    console.info(data)
-  })
+  child.stdout.on("data", function(data) {
+    console.info(data);
+  });
 
-  child.stderr.on('data', function(data) {
-    console.error(data)
-  })
+  child.stderr.on("data", function(data) {
+    console.error(data);
+  });
 
-  child.on('close', function(code) {
-    console.log(`child process close all stdio with code ${code}`)
-  })
+  child.on("close", function(code) {
+    console.log(`child process close all stdio with code ${code}`);
+  });
 
-  child.on('exit', function(code) {
-    console.log(`child process exited with code ${code}`)
-  })
+  child.on("exit", function(code) {
+    console.log(`child process exited with code ${code}`);
+  });
 
-  process.on('exit', function(code) {
-    console.log(`stack-env exited with code ${code}`)
-    child.kill()
-  })
+  process.on("exit", function(code) {
+    console.log(`stack-env exited with code ${code}`);
+    child.kill();
+  });
 }
 
-args.option('cmd', 'The command to run')
-args.option('file', 'Choose an env file')
-args.option('verbose', 'Print full logs')
+args.option("cmd", "The command to run");
+args.option("file", "Choose an env file");
+args.option(["v", "verbose"], "Print full logs");
+args.option(["k", "key"], "Stack key");
 
-const flags = args.parse(process.argv)
+const flags = args.parse(process.argv);
 if (flags.cmd) {
-  cmd(flags)
+  cmd(flags);
 }
-
